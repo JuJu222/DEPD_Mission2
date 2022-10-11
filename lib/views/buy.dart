6b06d90d202e30b7d6cfb201bb8346bfc9489736
JuJu222 <japjustin@yahoo.com>
2022-10-11 20:04:59 +0700

    Finished project

--- lib/views/buy.dart
+++ lib/views/buy.dart
@@ -29,80 +29,146 @@ class _BuyState extends State<Buy> {
   }
 
   void handleFormSubmit() {
-    showDialog(
-        context: context,
-        builder: (_) => AlertDialog(
-              title: const Text('Dialog Title'),
-              content: const Text('This is my content'),
-              actions: [
-                TextButton(
-                  child: const Text("OK"),
-                  onPressed: () {
-                    Navigator.of(context, rootNavigator: true).pop('dialog');
-                  },
+    if (_formKey.currentState!.validate()) {
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: const Text('Purchase Confirmation'),
+                content: Wrap(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Name: ${ctrlFullName.text}'),
+                        Text('Email: ${ctrlEmail.text}'),
+                        Text('Phone: ${ctrlPhone.text}'),
+                        Text('City: ${ctrlCity.text}'),
+                      ],
+                    ),
+                  ],
                 ),
-              ],
-            ));
+                actions: [
+                  TextButton(
+                    child: const Text("OK"),
+                    onPressed: () {
+                      Navigator.of(context).popUntil((route) => route.isFirst);
+                    },
+                  ),
+                ],
+              ));
+    } else {
+      showDialog(
+          context: context,
+          builder: (_) => AlertDialog(
+                title: const Text('Purchase Failed'),
+                content: const Text('Please re-check all the input fields!'),
+                actions: [
+                  TextButton(
+                    child: const Text("OK"),
+                    onPressed: () {
+                      Navigator.of(context, rootNavigator: true).pop('dialog');
+                    },
+                  ),
+                ],
+              ));
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login Page"),
-      ),
-      body: Container(
-        padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    labelText: "Full Name", prefixIcon: Icon(Icons.person)),
-                controller: ctrlFullName,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (value) {},
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    labelText: "Email", prefixIcon: Icon(Icons.email)),
-                controller: ctrlEmail,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (value) {},
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    labelText: "Phone Number", prefixIcon: Icon(Icons.phone)),
-                controller: ctrlPhone,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (value) {},
-              ),
-              const SizedBox(height: 16),
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    labelText: "City", prefixIcon: Icon(Icons.location_city)),
-                controller: ctrlCity,
-                autovalidateMode: AutovalidateMode.onUserInteraction,
-                validator: (value) {},
-              ),
-              const SizedBox(height: 32),
-              ElevatedButton(
-                onPressed: () {
-                  handleFormSubmit();
-                },
-                child: const Text('Login'),
-              )
-            ],
-          ),
+        appBar: AppBar(
+          backgroundColor: Colors.red.shade300,
+          title: const Text('Purchase Form',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         ),
-      ),
-    );
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(24, 16, 24, 16),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.name,
+                    decoration: const InputDecoration(
+                        labelText: "Full Name", prefixIcon: Icon(Icons.person)),
+                    controller: ctrlFullName,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      return value.toString().length < 6
+                          ? 'Full name must contain more than 5 letters!'
+                          : null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: const InputDecoration(
+                        labelText: "Email", prefixIcon: Icon(Icons.email)),
+                    controller: ctrlEmail,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      return !EmailValidator.validate(value!)
+                          ? 'Email is not complete!'
+                          : null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    keyboardType: TextInputType.number,
+                    decoration: const InputDecoration(
+                        labelText: "Phone Number",
+                        prefixIcon: Icon(Icons.phone)),
+                    controller: ctrlPhone,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      return value.toString().length < 9
+                          ? 'Phone number must contain more than 8 numbers!'
+                          : null;
+                    },
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    keyboardType: TextInputType.text,
+                    decoration: const InputDecoration(
+                        labelText: "City",
+                        prefixIcon: Icon(Icons.location_city)),
+                    controller: ctrlCity,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    validator: (value) {
+                      return value.toString().length < 4
+                          ? 'City must contain more than 3 letters!'
+                          : null;
+                    },
+                  ),
+                  const SizedBox(height: 32),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        minimumSize: const Size.fromHeight(40)),
+                    onPressed: () {
+                      handleFormSubmit();
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.shopping_cart_checkout,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'Buy Now',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
